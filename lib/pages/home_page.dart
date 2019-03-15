0d50301d19b5855d3654c06160330cc5def6298a
FelixBin <1948728748@qq.com>
2019-03-15 23:08:01 +0800

    m顶部首页分类导航

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';//轮播组件
-import 'dart:convert';//json
-import 'package:flutter_screenutil/flutter_screenutil.dart';//屏幕适配
+import 'package:flutter_swiper/flutter_swiper.dart'; //轮播组件
+import 'dart:convert'; //json
+import 'package:flutter_screenutil/flutter_screenutil.dart'; //屏幕适配
 
 class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
@@ -20,9 +20,12 @@ class _HomePageState extends State<HomePage> {
               var data = json.decode(snapshot.data.toString());
               List<Map> swiperDataList =
                   (data['data']['slides'] as List).cast();
+              List<Map> navigatorList =
+                  (data['data']['category'] as List).cast(); //类别列表
               return Column(
                 children: <Widget>[
                   SwiperDiy(swiperDataList: swiperDataList),
+                  TopNavigator(navigatorList: navigatorList), //导航组件
                 ],
               );
             } else {
@@ -37,23 +40,69 @@ class _HomePageState extends State<HomePage> {
 
 //轮播
 class SwiperDiy extends StatelessWidget {
-
   final List swiperDataList;
   SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.instance=ScreenUtil(width: 750,height: 1334)..init(context);//初始化尺寸
     return Container(
-      height: ScreenUtil().setHeight(333),//设置尺寸
+      height: ScreenUtil().setHeight(333), //设置尺寸
       width: ScreenUtil().setWidth(750),
       child: Swiper(
         itemBuilder: (BuildContext context, int index) {
-          return Image.network("${swiperDataList[index]['image']}",fit: BoxFit.cover,);
+          return Image.network(
+            "${swiperDataList[index]['image']}",
+            fit: BoxFit.cover,
+          );
         },
         itemCount: swiperDataList.length,
         pagination: new SwiperPagination(),
         autoplay: true,
+      ),
+    );
+  }
+}
+
+//首页导航(单组件)
+class TopNavigator extends StatelessWidget {
+  //这个类被调用时会传递一个list过来
+  final List navigatorList;
+
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (navigatorList.length > 0) {
+      navigatorList.removeRange(10, navigatorList.length);// navigatorList.removeLast();
+    }
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: const EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: const EdgeInsets.all(5.0),
+        children: navigatorList.map((item) {
+          return _gridViewItemUI(context, item); //每一个列表项
+        }).toList(),
+      ),
+    );
+  }
+
+//上图下文结构
+  Widget _gridViewItemUI(BuildContext context, item) {
+    return InkWell(
+      //路由跳转
+      onTap: () {
+        print('导航');
+      },
+      child: Column(
+        children: <Widget>[
+          Image.network(
+            item['image'],
+            width: ScreenUtil().setWidth(95),
+          ),
+          Text(item['mallCategoryName'])
+        ],
       ),
     );
   }
